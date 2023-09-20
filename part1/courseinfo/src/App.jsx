2b8b9a5a46f19d92db3e179c09638207c155cdcd
rickeymaharjan <rickeymhrjn23@gmail.com>
2023-09-20 13:33:31 +0545

    courseinfo ex1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,12 +6,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <>
+    <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+
 const Content = (props) => {
   return (
     <>
-      <p>{props.part[0]} {props.exercises[0]}</p>
-      <p>{props.part[1]} {props.exercises[1]}</p>
-      <p>{props.part[2]} {props.exercises[2]}</p>
+      <Part part={props.part[0]} exercises={props.exercises[0]}/>
+      <Part part={props.part[1]} exercises={props.exercises[1]}/>
+      <Part part={props.part[2]} exercises={props.exercises[2]}/>
     </>
   )
 }
