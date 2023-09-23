74312894275791e32f562652c4c428c9baca042c
rickeymaharjan <rickeymhrjn23@gmail.com>
2023-09-23 13:57:37 +0545

    courseinfo ex1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -32,26 +32,30 @@ const Total = (props) => {
   )
 }
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const App = () => {
+    const course = 'Half Stack application development'
+    const parts = [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+
+  
 
     return (
     <div>
       <Header name={course}/>
-      <Content part={[part1, part2, part3]}/>
-      <Total part={[part1, part2, part3]}/>
+      <Content part={parts}/>
+      <Total part={parts}/>
     </div>
   )
 }
