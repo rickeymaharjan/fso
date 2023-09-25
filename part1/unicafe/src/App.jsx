332feff99fea6e5a43ed4bda7c80aea9d8adfa0f
rickeymaharjan <rickeymhrjn23@gmail.com>
2023-09-25 11:23:21 +0545

    ex1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,17 +9,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
 
   return (
     <div>
       <Title text="give feedback"/>
-      <Button handleClick={() => setGood(good + 1)} text="good"/>
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
-      <Button handleClick={() => setBad(bad + 1)} text="bad"/>
+      <Button handleClick={() => {
+        setGood(good + 1)
+        setTotal(total + 1)
+          }} text="good"/>
+
+      <Button handleClick={() => {
+        setNeutral(neutral + 1)
+        setTotal(total + 1)
+          }} text="neutral"/>
+
+      <Button handleClick={() => {
+        setBad(bad + 1)
+        setTotal(total + 1)
+          }} text="bad"/>
+
       <Title text="statistics"/>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {((good - bad)/total)}</p>
+      <p>postive {(good/total) * 100} %</p>
     </div>
   )
 }
