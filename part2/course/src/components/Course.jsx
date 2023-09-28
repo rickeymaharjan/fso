3be2ed92642d897f17ba3b0f68a17f2f8037bc1e
rickeymaharjan <rickeymhrjn23@gmail.com>
2023-09-28 07:56:06 +0545

    courseinfo ex2.5

--- part2/course/src/components/Course.jsx
+++ part2/course/src/components/Course.jsx
@@ -2,14 +2,16 @@ import Header from "./Header"
 import Content from "./Content"
 import Total from "./Total"
 
-const Course = ({course}) => {
+const Course = ({courses}) => {
     return (
-        <>
-            <Header course={course}/>
-            <Content parts={course.parts}/>
-            <Total parts={course.parts}/>
-        </>
-    )
-}
+        courses.map(course => (
+            <div key={course.id}>
+                <Header course={course}/>
+                <Content parts={course.parts}/>
+                <Total parts={course.parts}/>
+            </div>
+            ))
+        )
+    }
 
 export default Course
